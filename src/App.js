d38a2fb55ce7946cc500fc0f18b0e5ec4499bfc9
Guruprashanth98 <guruprashanth78@gmail.com>
2021-03-15 15:16:38 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './Pages/Login'
+import { BrowserRouter as Router,Route, Switch, Redirect } from 'react-router-dom';
+import Landing from './Pages/Landing'
+import Topbar from './Components/Topbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Topbar  />
+          <Switch>
+            <Route exact path = "/:user/landing" component = {Landing}/>
+            <Route exact  path  = "/"  component  = {Login} />
+          </Switch>
+      </Router>
   );
 }
 
